b7dabd0fdd034b39c0414099175e429538d38bb4
kenmaro3 <miha.ken.19@gmail.com>
2022-11-20 21:30:29 +0900

    [UPD] tmp today

--- pages/nomi/confirm.js
+++ pages/nomi/confirm.js
@@ -115,87 +115,86 @@ function Confirm() {
           })
       }
 
-    }
 
     });
-}
+  }
 
 
 
 
 
 
-return (
-  <div className="flex flex-col h-screen justify-center items-center">
-    {!modal ?
-      <div>
-        <label className="block">
-          <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-            Confirmation
-          </span>
-          <div className="flex flex-col">
-            <div>name: {nomi.name}</div>
-            <div>date:
-              {nomi.date?.map((el) => (<div>{ts_to_date(el)}</div>))}
-
-            </div>
-            <div>location:
-              {nomi.location?.map((el) => (<div>{el}</div>))}
-            </div>
-            <div>deadline:
-              {/* {nomi.deadline?.map((el) => (<div>{ts_to_date(el)}</div>))} */}
-              <div>{ts_to_date(nomi.deadline)}</div>
-
-            </div>
-            {/* <div>passcode: {nomi.passcode}</div> */}
-          </div>
-        </label>
-
+  return (
+    <div className="flex flex-col h-screen justify-center items-center">
+      {!modal ?
         <div>
-          <button
-            onClick={(e) => goToModify(e)}
-            className="bg-sky-500 hover:bg-sky-700 py-2 px-4 rounded text-white max-w-xs"
-          >
-            Modify
-          </button>
+          <label className="block">
+            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+              Confirmation
+            </span>
+            <div className="flex flex-col">
+              <div>name: {nomi.name}</div>
+              <div>date:
+                {nomi.date?.map((el) => (<div>{ts_to_date(el)}</div>))}
 
-          <button
-            onClick={(e) => goToOk(e)}
-            className="bg-sky-500 hover:bg-sky-700 py-2 px-4 rounded text-white max-w-xs"
-          >
-            Ok
-          </button>
+              </div>
+              <div>location:
+                {nomi.location?.map((el) => (<div>{el}</div>))}
+              </div>
+              <div>deadline:
+                {/* {nomi.deadline?.map((el) => (<div>{ts_to_date(el)}</div>))} */}
+                <div>{ts_to_date(nomi.deadline)}</div>
+
+              </div>
+              {/* <div>passcode: {nomi.passcode}</div> */}
+            </div>
+          </label>
+
+          <div>
+            <button
+              onClick={(e) => goToModify(e)}
+              className="bg-sky-500 hover:bg-sky-700 py-2 px-4 rounded text-white max-w-xs"
+            >
+              Modify
+            </button>
+
+            <button
+              onClick={(e) => goToOk(e)}
+              className="bg-sky-500 hover:bg-sky-700 py-2 px-4 rounded text-white max-w-xs"
+            >
+              Ok
+            </button>
+          </div>
+
         </div>
-
-      </div>
-      :
-      <div className="relative w-full max-w-md h-full md:h-auto">
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          {/* <button onClick={(e) => closeModal(e)} type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="authentication-modal">
+        :
+        <div className="relative w-full max-w-md h-full md:h-auto">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            {/* <button onClick={(e) => closeModal(e)} type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="authentication-modal">
               <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
               <span className="sr-only">Close modal</span>
             </button> */}
-          <div className="py-6 px-6 lg:px-8">
-            <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white"></h3>
-            <div>share url</div>
-            <div>{shareUrl}}</div>
+            <div className="py-6 px-6 lg:px-8">
+              <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white"></h3>
+              <div>share url</div>
+              <div>{shareUrl}}</div>
 
-            <div>passcode</div>
-            <div>{passcode}</div>
-            {/* <form className="space-y-6" action="#">
+              <div>passcode</div>
+              <div>{passcode}</div>
+              {/* <form className="space-y-6" action="#">
                 <div>
                   <label for="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter passcode</label>
                   <input value={passcode} onChange={handlePasscodeChange} type="text" name="passcode" id="passcode" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
                 </div>
                 <button onClick={(e) => goBackToPlan(e)} type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Go Back</button>
               </form> */}
-            <button onClick={(e) => goBackToPlan(e)} type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Go Back</button>
+              <button onClick={(e) => goBackToPlan(e)} type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Go Back</button>
+            </div>
           </div>
         </div>
-      </div>
-    }
-  </div>
-);
+      }
+    </div>
+  );
 }
 
 export default Confirm;
