b935352bd2acd31e6f3147468d593710657e98e5
kenmaro3 <miha.ken.19@gmail.com>
2022-11-25 13:53:07 +0900

    [UPD] logo

--- components/header.js
+++ components/header.js
@@ -4,7 +4,8 @@ import Link from "next/link"
 function Header() {
     return (
         <Link href="/">
-            <h1 className="cursor-pointer text-xl font-bold px-4 py-2 shadow-sm mb-3"><span className="mr-1">🍺</span>ノミカイ・カンジ</h1>
+            {/* <h1 className="cursor-pointer text-xl font-bold px-4 py-2 shadow-sm mb-3"><span className="mr-1">🍺</span>ノミカイ・カンジ</h1> */}
+            <img className='cursor-pointer h-16' src="logo.png"></img>
         </Link>
     )
 }
